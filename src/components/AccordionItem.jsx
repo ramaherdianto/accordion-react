d34dbe03c069dad03cec9be5dc0c93c93530216c
ramaherdianto <ramadhanherdianto35@gmail.com>
2024-08-05 19:08:25 +0700

    fix: fixed UI

--- src/components/AccordionItem.jsx
+++ src/components/AccordionItem.jsx
@@ -10,25 +10,20 @@ const AccordionItem = ({ faq, currOpen, setIsOpen }) => {
     return (
         <>
             <article
-                className={`flex cursor-pointer flex-col sm:max-w-2xl p-4 rounded-lg justify-center gap-8 transition-all duration-300 ease-in-out ${
+                className={`flex cursor-pointer sm:max-w-2xl p-4 rounded-lg justify-start gap-8 transition-all duration-300 ease-in-out ${
                     isOpen ? 'bg-[#F0F4FD]' : ''
                 }`}
                 onClick={handleToggle}
             >
-                <div className='flex gap-4 items-center'>
+                <div className='flex gap-4 items-start'>
                     <span className='text-xl font-semibold bg-white w-14 h-7 rounded-full flex items-center justify-center'>
                         {isOpen ? '-' : '+'}
                     </span>
-                    <h1 className='text-md font-semibold'>{faq.question}</h1>
                 </div>
-                {isOpen && (
-                    <div className='flex justify-center items-center gap-4'>
-                        <div className='text-xl font-semibold invisible bg-white w-[90px] sm:w-[100px] md:w-[95px] lg:w-[85px] h-7 rounded-full flex items-center justify-center'>
-                            +
-                        </div>
-                        <p className='text-sm'>{faq.answer}</p>
-                    </div>
-                )}
+                <div className='flex flex-col justify-center items-start gap-4'>
+                    <h1 className='text-md font-semibold ms-0'>{faq.question}</h1>
+                    {isOpen && <p className='text-sm'>{faq.answer}</p>}
+                </div>
             </article>
         </>
     );
